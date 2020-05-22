56503b7941672785a5b70a9e12584268536488c7
Raul Cisneros <raul.f.cisneros@outlook.com>
2020-05-22 13:34:48 -0700

    Finalized

--- Program.cs
+++ Program.cs
@@ -1,43 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace HW2_Cisneros
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)  ///This is very important Method
         {
-            WriteAverage();
-            WriteEuropean();
-            WriteAmerican();
+            WriteAverage();     //Method Calls
+            WriteAmerican();    //Method Calls
+            WriteEuropean();    //Method Calls
+
         }
 
-
+        // This is the method definition for WriteAverage
         static void WriteAverage()
         {
             double num1 = 2.0;
             double num2 = 7;
             Console.WriteLine((num1 + num2) / 2);
         }
-
+        // This is the method definition for WriteAmerican
         static void WriteAmerican()
         {
-            string day = "Saturday";
+            string day = "Saturday,";
             int date = (18);
             string month = "July";
             int year = (2020);
             Console.WriteLine(day + " " + month + " " + date + "," + " " + year);
         }
 
-
+        // This is the method definition for WriteEuropean
         static void WriteEuropean()
         {
-
-
-            string day = "Saturday,";
+            string day = "Saturday";
             string month = "July";
             int date = (18);
             int year = (2020);
